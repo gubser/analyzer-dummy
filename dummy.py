142dde27582839fd68c94b6a999a3ad8d791e501
Elio Gubser <gubser@users.noreply.github.com>
2016-06-24 14:47:18 +0200

    fixed until works

--- dummy.py
+++ dummy.py
@@ -1,25 +1,38 @@
 """
 This analyzer reads line-seperated text files and every line is stored as a dummy observation with the line as its value.
 """
+import dateutil.parser
 
 from ptocore.analyzercontext import AnalyzerContext
 
 ac = AnalyzerContext()
+
+max_action_id, timespans = ac.sensitivity.basic()
+ac.set_result_info(max_action_id, timespans)
 
 uploads = ac.spark_uploads(['dummy'])
 
 def create_docs(kv):
     filename, (metadata, data) = kv
 
-    start_time = metadata['meta.start_time']
+    # HACK because pymongo-hadoop cannot serialize datetime and objectid
+    # store as string now then transform into datetime later
+    start_time = metadata['meta']['start_time'].isoformat()
+    sources = [metadata['action_id']]
+
     for line in data.split(b'\n'):
-        doc = {
+        yield {
             'condition': 'dummy',
             'time': start_time,
             'path': ['*'],
             'value': line.decode('utf-8'),
-            'sources': {'rawdata': metadata['_id']},
-            'analyzer_id': 'dummy'
+            'sources': sources,
+            'analyzer_id': 'analyzer-dummy'
         }
 
 uploads.flatMap(create_docs).saveToMongoDB(ac.temporary_uri)
+
+# HACK see above.
+todo = list(ac.temporary_coll.find({}, {'_id': 1, 'time': 1}))
+for obs in todo:
+    ac.temporary_coll.update_one({'_id': obs['_id']}, {'$set': {'time': dateutil.parser.parse(obs['time'])}})
